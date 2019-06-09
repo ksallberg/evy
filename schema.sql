a3929530a5d7e466aa2a18ed8be1ffff68bdae3e
Kristian Sallberg <kristian@purestyle.se>
2019-06-09 17:34:35 +0200

    insert dummy data

--- schema.sql
+++ schema.sql
@@ -21,6 +21,14 @@ CREATE TABLE Account (
   PRIMARY KEY (username)
 );
 
+INSERT INTO Account (username, email, encrypted_password)
+  VALUES ('kris', 'k@p.s', 'lol');
+
+INSERT INTO Account (username, email, encrypted_password)
+  VALUES ('elin', 'e@g.c', 'olo');
+
+SELECT * FROM Account;
+
 CREATE TABLE Portfolio (
   id SERIAL,
   name varchar(20) NOT NULL UNIQUE,
@@ -28,6 +36,13 @@ CREATE TABLE Portfolio (
   PRIMARY KEY (id),
   FOREIGN KEY (owner) REFERENCES Account(username)
 );
+
+INSERT INTO Portfolio (name, owner) VALUES ('krisport1', 'kris');
+INSERT INTO Portfolio (name, owner) VALUES ('krisport2', 'kris');
+INSERT INTO Portfolio (name, owner) VALUES ('elinport1', 'elin');
+INSERT INTO Portfolio (name, owner) VALUES ('elinport2', 'elin');
+
+SELECT * FROM Portfolio;
 
 CREATE TABLE Entry (
   portfolio_id int NOT NULL,
@@ -39,3 +54,11 @@ CREATE TABLE Entry (
   PRIMARY KEY (portfolio_id, ts),
   FOREIGN KEY (portfolio_id) REFERENCES Portfolio(id)
 );
+
+INSERT INTO Entry (portfolio_id, symbol, type, units, price, ts)
+  VALUES (1, 'msft', 'buy', 20, 30.3, '2019-04-20 12:00:00');
+
+INSERT INTO Entry (portfolio_id, symbol, type, units, price, ts)
+  VALUES (4, 'mdca', 'buy', 100, 2.4, '2019-06-07 13:12:00');
+
+SELECT * FROM Entry;
